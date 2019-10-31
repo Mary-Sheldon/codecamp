3bdce36f42a640289f60adc94459e3e4ce8b8c71
Catherine <netsurfer1193@gmail.com>
2019-10-31 11:15:46 -0400

    Cheese MVC studio check boxes not drop down

--- cheese-mvc/src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ cheese-mvc/src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -8,31 +8,60 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
-    static ArrayList<String> cheeses = new ArrayList<>();
+
+    public static void main(String[] args) {
+    }
+
+    HashMap<String, String> cheeses = new HashMap<>();
+
+    {
+    }
+
     @RequestMapping(value = "")
     public String index(Model model) {
-
 
         model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
 
         return "cheese/index";
     }
-    @RequestMapping(value= "add", method = RequestMethod.GET)
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddCheeseForm(Model model) {
-        model.addAttribute ("title", "Add Cheese");
+        model.addAttribute("title", "Add Cheese");
         return "cheese/add";
 
     }
-    @RequestMapping(value= "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName){
 
-      cheeses.add(cheeseName);
-      //rediret to cheese
-      return "redirect:";
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
+
+        cheeses.put(cheeseName, cheeseDescription);
+        //rediret to cheese
+        return "redirect:";
     }
-}
+
+
+    @RequestMapping (value = "delete", method = RequestMethod.GET)
+    public  String RemoveCheeseController (Model model){
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title","Remove Cheese");
+        return "cheese/delete";
+        }
+
+
+
+    @RequestMapping(value = "delete", method = RequestMethod.POST)
+    public String RemoveCheeseController(@RequestParam ArrayList<String> cheeseName) {
+        for (String cheese  : cheeseName ){
+            cheeses.remove(cheese);
+        }
+        return "redirect:";
+        }
+
+        }
